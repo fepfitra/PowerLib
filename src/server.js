79ed6e3da47620e6f31c22153b4ae238a8803e85
fitrafepUGM <fitriansyahekaputra2001@mail.ugm.ac.id>
2022-11-26 22:01:48 +0700

    link to route file

--- src/server.js
+++ src/server.js
@@ -1,12 +1,15 @@
 /* eslint-disable no-console */
 
 const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
 
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
     host: 'localhost',
   });
+
+  server.route(routes);
 
   await server.start();
   console.log('Server running on %s', server.info.uri);
